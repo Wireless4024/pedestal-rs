c1d0c129dd02b5eafb7c8edffc0c65568c3b868b
wireless4024 <wireless4024@proton.me>
2022-11-22 22:15:51 +0700

    feat: compute relative path

--- src/fs/path.rs
+++ src/fs/path.rs
@@ -1,3 +1,4 @@
+use std::fs::canonicalize;
 use std::io;
 use std::io::ErrorKind;
 use std::path::{Component, Path, PathBuf};
@@ -41,11 +42,37 @@ pub fn normalize(base: &Path, p: impl AsRef<Path>) -> io::Result<PathBuf> {
 	Ok(buf)
 }
 
+/// Get relative path to access source from target
+pub fn relative_from(source: impl AsRef<Path>, target: impl AsRef<Path>) -> PathBuf {
+	let source: &Path = source.as_ref();
+	let mut target: &Path = target.as_ref();
+	// if absolute; replace with relative location from root
+	if target.is_absolute() {
+		target = target.strip_prefix(canonicalize(".").unwrap()).unwrap();
+	}
+	let mut scomp = source.components().peekable();
+	let mut tcomp = target.components().peekable();
+
+	let mut relative = PathBuf::new();
+
+	while let Some(comp) = scomp.peek() {
+		if Some(comp) == tcomp.peek() {
+			scomp.next();
+			tcomp.next();
+			continue;
+		} else { break; }
+	}
+
+	for _ in tcomp { relative.push("../"); }
+	for x in scomp { relative.push(x); }
+	relative
+}
+
 #[cfg(test)]
 mod tests {
 	use std::path::PathBuf;
 
-	use crate::fs::path::normalize;
+	use crate::fs::path::{normalize, relative_from};
 
 	#[test]
 	fn test_normalize() {
@@ -58,5 +85,12 @@ mod tests {
 		want.push("../");
 		let result = normalize(&base, &want);
 		assert!(result.is_err())
+	}
+
+	#[test]
+	fn test_absolute_from1() {
+		assert_eq!(relative_from("src/lib.rs", "target/debug"), PathBuf::from("../../src/lib.rs"));
+		assert_eq!(relative_from("src/lib.rs", "src/fs"), PathBuf::from("../lib.rs"));
+		assert_eq!(relative_from("../src/lib.rs", "target/debug"), PathBuf::from("../../../src/lib.rs"));
 	}
 }
